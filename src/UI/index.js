6bcbc2746955c15499770980d452241e4132e57d
yinzhao <yinzhao@live.cn>
2021-04-23 18:37:00 +0800

    updata Message

--- src/UI/index.js
+++ src/UI/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
-import {Button,Input,Checkbox,Row,Col} from 'element-ui'
-const UIARRAY = [Button,Input,Checkbox,Row,Col]
+import {Button,Input,Checkbox,Row,Col, Message} from 'element-ui'
+const UIARRAY = [Button,Input,Checkbox,Row,Col,Message]
 // import ElementUI from 'element-ui'
 // import 'element-ui/lib/theme-chalk/index.css'
 // Vue.use(ElementUI)
+Vue.prototype.$message = Message
 UIARRAY.forEach(item=>{
   Vue.use(item)
 })
